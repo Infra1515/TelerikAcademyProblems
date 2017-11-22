c7d0edd9e105d41f97951259cb84398e583c27b7
Lubomir Yonchev <infra1515@gmail.com>
2017-11-22 12:15:33 +0200

    Second commit

---
+++ TelerikAlphaExams/TelerikEntranceExam11Sep/TelerikEntranceExam11Sep/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace TelerikAcademyFundamentals
+{
+    class TelerikAcademyC_Sharp_Fund_2012_2013
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine(); ;
+            var list = input.Split(' ').Select(Int64.Parse).ToList();
+            BigInteger sum = 0;
+            int index = 1;
+            while (index < list.Count())
+            {
+                long current = list[index];
+                long previous = list[index - 1];
+                long diff = Math.Abs(current - previous);
+                if (diff % 2 == 0)
+                {
+                    index += 2;
+                    sum += diff;
+                }
+                else
+                {
+                    index += 1;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
